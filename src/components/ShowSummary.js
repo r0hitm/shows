96b7b04758579cdb95490178e931bb543efc9160
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-05-06 13:56:05 +0530

    WIP

--- src/components/ShowSummary.js
+++ src/components/ShowSummary.js
@@ -3,11 +3,9 @@ export default function ShowSummary({ show, hideSummary }) {
         <div className="container">
             <h1>{show.show.name}</h1>
             <img src={show.show.image?.medium} alt="" />
-            <p>{show.show.summary}</p>
-            <button
-                className="btn btn-primary"
-                onClick={hideSummary}
-            >
+            {/* The show summary is formatted using html tags so don't need to explicity use <p> here */}
+            <div dangerouslySetInnerHTML={{ __html: show.show.summary }}></div>
+            <button className="btn btn-primary" onClick={hideSummary}>
                 Hide Summary
             </button>
         </div>
