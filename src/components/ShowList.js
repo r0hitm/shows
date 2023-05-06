f97405efa3e318376f48b125e0543b09fa646ba7
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-05-06 15:14:06 +0530

    new styles and show brief details

--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -1,17 +1,33 @@
+import "../styles/ShowList.css";
+
 export default function ShowList({ shows, handleShowSummary }) {
     return (
-        <ul className="list-group">
+        <ul className="show-list">
             {shows.map(show => (
                 <li className="list-group-item" key={show.show.id}>
-                    <h2>{show.show.name}</h2>
-                    <img src={show.show.image?.medium} alt="" />
-                    {/* <p>{show.show.summary}</p> */}
-                    <button
-                        className="btn btn-primary"
-                        onClick={_ => handleShowSummary(show.show.id)}
-                    >
-                        Show Summary
-                    </button>
+                    <img
+                        className="show-img"
+                        src={show.show.image?.medium}
+                        alt="Show Thumbnail"
+                    />
+                    <div className="show-brief">
+                        <h2 className="show-name">{show.show.name}</h2>
+                        <div className="show-rating">
+                            <span>Rating: {show.show.rating.average}</span>
+                        </div>
+                        <div className="show-genres">
+                            <span>Genres: {show.show.genres.join(", ")}</span>
+                        </div>
+                        <div className="show-language">
+                            <span>Language: {show.show.language}</span>
+                        </div>
+                        <button
+                            className="btn btn-primary"
+                            onClick={_ => handleShowSummary(show.show.id)}
+                        >
+                            Show Summary
+                        </button>
+                    </div>
                 </li>
             ))}
         </ul>
